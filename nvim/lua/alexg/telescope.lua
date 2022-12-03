1ab6a4b861c4eee5f7d4d137dbf05c9b9198ee78
Alex Grekulovic <alex.grekulovic91@gmail.com>
2022-12-03 12:22:07 -0500

    more updates

--- nvim/lua/alexg/telescope.lua
+++ nvim/lua/alexg/telescope.lua
@@ -1,10 +1,3 @@
-local nnoremap = require('alexg.keymap').nnoremap
-
-nnoremap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
-nnoremap("<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
-nnoremap("<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
-nnoremap("<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
-
 require('telescope').setup{  defaults = { file_ignore_patterns = { "target" }} }
 
 
