047d1549d8351820008f23546a5cbf877ac66265
Alex Grekulovic <alex.grekulovic91@gmail.com>
2022-12-23 10:51:20 -0500

    update text

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -4,5 +4,5 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("search: ") });
+	builtin.grep_string({ search = vim.fn.input("Search string: ") });
 end)
