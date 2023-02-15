6f60257988883b0fc2b6a3c9f8f171eadefb0749
Alex Grekulovic <alex.grekulovic91@gmail.com>
2023-02-15 00:06:30 -0500

    fix transparency in nvim

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,4 +1,14 @@
 vim.o.termguicolors = true
 vim.cmd [[ colorscheme onenord ]]
+
+-- transparent bg
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
+vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
+vim.api.nvim_set_hl(0, "SpecialKey", { bg = "none" })
+vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
