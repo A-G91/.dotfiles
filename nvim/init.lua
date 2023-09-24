fe430c1c497e8872258e710fcf10ac340eede00c
Alex <32986495+A-G91@users.noreply.github.com>
2023-09-23 22:02:26 -0400

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -376,6 +376,10 @@ mason_lspconfig.setup_handlers {
 local cmp = require 'cmp'
 
 cmp.setup {
+  window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
   mapping = cmp.mapping.preset.insert {
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
