460241c989fdb5af1b42f4aebd122b9776d22353
Alex <alex.grekulovic91@gmail.com>
2024-04-03 23:28:42 -0400

    add theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -603,37 +603,17 @@ require("lazy").setup({
 		-- change the command in the config to whatever the name of that colorscheme is
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-		"ellisonleao/gruvbox.nvim",
+		-- "ellisonleao/gruvbox.nvim",
+		"jesseleite/nvim-noirbuddy",
+		dependencies = {
+			{ "tjdevries/colorbuddy.nvim" },
+		},
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			-- Load the colorscheme here
-			require("gruvbox").setup({
-				terminal_colors = true,
-				undercurl = true,
-				underline = true,
-				bold = true,
-				italic = {
-					strings = false,
-					emphasis = false,
-					comments = false,
-					operators = false,
-					folds = false,
-				},
-				strikethrough = true,
-				invert_selection = false,
-				invert_signs = false,
-				invert_tabline = false,
-				invert_intend_guides = false,
-				inverse = true,
-				contrast = "",
-				palette_overrides = {},
-				overrides = {},
-				dim_inactive = false,
-				transparent_mode = false,
+			require("noirbuddy").setup({
+				preset = "slate",
 			})
-			vim.o.background = "dark"
-			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
 
