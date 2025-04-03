2acf30963ddf9b400263bfc54ec45afbdbe76504
alex <alex@alex>
2025-04-02 22:20:48 -0400

    find files with rg

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -113,10 +113,7 @@ return {
             notify_on_error = false,
             format_on_save = {
                 timeout_ms = 500,
-                lsp_fallback = true,
-            },
-            formatters_by_ft = {
-                lua = { "stylua" }
+                lsp_format = "fallback",
             },
         }
     }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,20 @@
-return {
-    -- Highlight, edit, and navigate code
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+return {}
+--
+-- return {
+-- Highlight, edit, and navigate code
+-- "nvim-treesitter/nvim-treesitter",
+-- build = ":TSUpdate",
+-- config = function()
+-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
-        ---@diagnostic disable-next-line: missing-fields
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "python", "go", "html", "lua", "markdown", "vim", "vimdoc", "rust" },
-            -- Autoinstall languages that are not installed
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
-}
+---@diagnostic disable-next-line: missing-fields
+--      require("nvim-treesitter.configs").setup({
+--          ensure_installed = { "python", "go", "html", "lua", "markdown", "vim", "vimdoc", "rust" },
+-- Autoinstall languages that are not installed
+--          auto_install = true,
+--          highlight = { enable = true },
+--          indent = { enable = true },
+--      })
+--  end,
+-- }
+--
