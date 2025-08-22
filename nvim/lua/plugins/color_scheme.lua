f25f0a30f69c170cb247c33ad843fbf20d9434c4
alex <alex.grekulovic91@gmail.com>
2025-08-21 23:35:52 -0400

    miniwinter

--- nvim/lua/plugins/color_scheme.lua
+++ nvim/lua/plugins/color_scheme.lua
@@ -1,11 +1,13 @@
 return {
+  'echasnovski/mini.hues',
+  version = false,
   -- "vinitkumar/oscura-vim",
   -- "eemed/sitruuna.vim",
-  "ellisonleao/gruvbox.nvim",
+  -- "ellisonleao/gruvbox.nvim",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    vim.o.background = "dark"
-    vim.cmd.colorscheme("gruvbox")
+    -- vim.o.background = "dark"
+    vim.cmd.colorscheme("miniwinter")
   end
 }
