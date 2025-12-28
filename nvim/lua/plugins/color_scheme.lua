2a412ca94a61da72217b1694d26072af9bef9b6d
alex <alex.grekulovic91@gmail.com>
2025-12-28 07:32:43 -0500

    remove harpoon

--- nvim/lua/plugins/color_scheme.lua
+++ nvim/lua/plugins/color_scheme.lua
@@ -1,13 +1,13 @@
 return {
-  'echasnovski/mini.hues',
+  "ellisonleao/gruvbox.nvim",
   version = false,
+  -- 'echasnovski/mini.hues',
   -- "vinitkumar/oscura-vim",
   -- "eemed/sitruuna.vim",
-  -- "ellisonleao/gruvbox.nvim",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     -- vim.o.background = "dark"
-    vim.cmd.colorscheme("miniwinter")
+    vim.cmd.colorscheme("gruvbox")
   end
 }
